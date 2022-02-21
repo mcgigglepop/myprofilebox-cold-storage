ae1c6970ff8138f0a1dbe5f708419fd4d776de0f
cristopher <christoph.alano@gmail.com>
2022-02-20 21:43:58 -0600

    modified login activity. added new java class for custom text input

--- MyProfileBox2/app/src/main/java/com/mcgigglepop/myprofilebox/LoginActivity.java
+++ MyProfileBox2/app/src/main/java/com/mcgigglepop/myprofilebox/LoginActivity.java
@@ -3,6 +3,9 @@ package com.mcgigglepop.myprofilebox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final PinEntryEditText txtPinEntry = (PinEntryEditText) findViewById(R.id.txt_pin_entry);
+        txtPinEntry.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().equals("123456")) {
+                    Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                } else if (s.length() == "123456".length()) {
+                    Toast.makeText(LoginActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
+                    txtPinEntry.setText(null);
+                }
+            }
+        });
+
     }
 }
