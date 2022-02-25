db7ac154c66795009e8040a816f57366b816f264
cristopher <christoph.alano@gmail.com>
2022-02-24 22:33:43 -0600

    the copy database method

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelper.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelper.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "table.db";
@@ -51,5 +56,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return true;
         }
         return false;
+    }
+
+    private void copyDatabase(){
+        try{
+            InputStream inputStream = context.getAssets().open(DATABASE_NAME);
+            String outFileName = DATABASE_PATH + DATABASE_NAME;
+            OutputStream outputStream = new FileOutputStream(outFileName);
+
+            byte[] b = new byte[1024];
+            int length;
+
+            while((length = inputStream.read(b)) > 0){
+                outputStream.write(b, 0, length);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
