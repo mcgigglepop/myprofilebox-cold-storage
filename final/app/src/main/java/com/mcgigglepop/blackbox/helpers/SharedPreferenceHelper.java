5baf57dce404526e70ced80183a8fa23547e15ca
cristopher <christoph.alano@gmail.com>
2022-02-24 22:38:42 -0600

    added the shared preference classes and some private strings for managing onboarding

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/SharedPreferenceHelper.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/SharedPreferenceHelper.java
@@ -1,4 +1,15 @@
 package com.mcgigglepop.blackbox.helpers;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPreferenceHelper {
+    SharedPreferences preference;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    int PRIVATE_MODE = 0;
+    private static final String PREF_NAME = "onboarding";
+    private static final String IS_FIRST_TIME_LAUNCH = "isFirstTimeOnboarding";
+    private static final String IS_REGISTERED = "isRegistered";
 }
