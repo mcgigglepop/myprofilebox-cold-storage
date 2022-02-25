d8dba003f08ff503847f0cede121ea37706d34ec
cristopher <christoph.alano@gmail.com>
2022-02-24 22:59:57 -0600

    added comments

--- final/app/src/main/java/com/mcgigglepop/blackbox/OnboardingActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/OnboardingActivity.java
@@ -25,12 +25,14 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         prefManager = new SharedPreferenceManager(this);
+
+        // check first time launch
         if (!prefManager.getFirstTimeLaunch()) {
             launchHomeScreen();
             finish();
         }
 
-        // Making notification bar transparent
+        // making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
@@ -56,6 +58,7 @@ public class OnboardingActivity extends AppCompatActivity {
         viewPager.setAdapter(myViewPagerAdapter);
         viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
 
+        // skip
         btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,6 +66,7 @@ public class OnboardingActivity extends AppCompatActivity {
             }
         });
 
+        // next
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
