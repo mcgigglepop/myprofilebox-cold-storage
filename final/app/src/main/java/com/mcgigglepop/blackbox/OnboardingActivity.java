6351bb54e964624c2031501e3f689884f91ea3f8
cristopher <christoph.alano@gmail.com>
2022-02-24 22:49:46 -0600

    added some private variables

--- final/app/src/main/java/com/mcgigglepop/blackbox/OnboardingActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/OnboardingActivity.java
@@ -1,10 +1,23 @@
 package com.mcgigglepop.blackbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mcgigglepop.blackbox.helpers.SharedPreferenceHelper;
 
 public class OnboardingActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private SharedPreferenceManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
