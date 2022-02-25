932e22931324bba87d2b33e6868ed8f02cca1b29
cristopher <christoph.alano@gmail.com>
2022-02-24 22:31:17 -0600

    extended sqliteopenhelper and added some private variables. Implemented helper methods

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelper.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelper.java
@@ -1,4 +1,26 @@
 package com.mcgigglepop.blackbox.helpers;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "table.db";
+    private static final int DATABASE_VERSION =  1;
+    private final Context context;
+    private static final String DATABASE_PATH = "/data/data/com.mcgigglepop.blackbox/databases/";
+    private static final String USER_TABLE = "user";
+
+    SQLiteDatabase db;
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
