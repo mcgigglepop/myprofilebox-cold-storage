a5053835081011da6b5c2ea35b4a5386fa3c12ae
cristopher <christoph.alano@gmail.com>
2022-02-24 22:53:21 -0600

    instantiated the preference manager, checked preference manager, added transparent notification bar

--- final/app/src/main/java/com/mcgigglepop/blackbox/OnboardingActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/OnboardingActivity.java
@@ -3,7 +3,9 @@ package com.mcgigglepop.blackbox;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,6 +24,17 @@ public class OnboardingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefManager = new SharedPreferenceManager(this);
+        if (!prefManager.getFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
         setContentView(R.layout.activity_onboarding);
     }
 }
