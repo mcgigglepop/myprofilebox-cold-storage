2490e45ca30fca850a90842949a7436519ee9e18
cristopher <christoph.alano@gmail.com>
2022-02-26 00:10:28 -0600

    minor edits

--- final/app/src/main/java/com/mcgigglepop/blackbox/RegisterActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/RegisterActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mcgigglepop.blackbox.helpers.CustomEditText;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final CustomEditText editTextRegister = findViewById(R.id.edit_text_register);
     }
 }
