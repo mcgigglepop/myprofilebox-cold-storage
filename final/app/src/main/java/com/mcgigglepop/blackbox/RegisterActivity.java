18ae85e62e6700cdf520edd4fbe3393129108849
cristopher <christoph.alano@gmail.com>
2022-02-26 00:25:02 -0600

    added on click listener for the register button

--- final/app/src/main/java/com/mcgigglepop/blackbox/RegisterActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/RegisterActivity.java
@@ -3,15 +3,28 @@ package com.mcgigglepop.blackbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mcgigglepop.blackbox.helpers.CustomEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         final CustomEditText editTextRegister = findViewById(R.id.edit_text_register);
+
+        registerButton = (Button) findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
