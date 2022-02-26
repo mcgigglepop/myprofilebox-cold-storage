5b3bf6bd968452b65b77832052e32fbdd0d31a8d
cristopher <christoph.alano@gmail.com>
2022-02-25 23:58:26 -0600

    added variables

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/CustomEditText.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/CustomEditText.java
@@ -1,4 +1,30 @@
 package com.mcgigglepop.blackbox.helpers;
 
-public class CustomEditText {
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class CustomEditText extends androidx.appcompat.widget.AppCompatEditText{
+
+    public static final String XML_NAMESPACE_ANDROID = "http://schemas.android.com/apk/res/android";
+    private float mSpace = 24; //24 dp by default, space between the lines
+    private float mCharSize;
+    private float mNumChars = 6;
+    private float mLineSpacing = 8; //8dp by default, height of the text from our lines
+    private int mMaxLength = 6;
+    private OnClickListener mClickListener;
+    private float mLineStroke = 1; //1dp by default
+    private float mLineStrokeSelected = 2; //2dp by default
+    private Paint mLinesPaint;
+
+    int[][] mStates = new int[][]{
+            new int[]{android.R.attr.state_selected}, // selected
+            new int[]{android.R.attr.state_focused}, // focused
+            new int[]{-android.R.attr.state_focused}, // unfocused
+    };
+
+    int[] mColors = new int[]{
+            Color.GREEN,
+            Color.BLACK,
+            Color.GRAY
+    };
 }
