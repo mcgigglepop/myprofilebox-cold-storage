1549a618f2e21085b66a6f284b26c3360631e9e2
cristopher <christoph.alano@gmail.com>
2022-02-26 00:40:02 -0600

    added accounts activity

---
+++ final/app/src/main/java/com/mcgigglepop/blackbox/AccountsActivity.java
@@ -0,0 +1,14 @@
+package com.mcgigglepop.blackbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accounts);
+    }
+}
