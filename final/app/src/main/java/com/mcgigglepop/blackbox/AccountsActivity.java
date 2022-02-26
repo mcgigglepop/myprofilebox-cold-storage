f9e8df8c650eddda05b689776d81a42b6aab49dc
cristopher <christoph.alano@gmail.com>
2022-02-26 01:21:24 -0600

    added floating action button

--- final/app/src/main/java/com/mcgigglepop/blackbox/AccountsActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/AccountsActivity.java
@@ -3,12 +3,27 @@ package com.mcgigglepop.blackbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AccountsActivity extends AppCompatActivity {
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
