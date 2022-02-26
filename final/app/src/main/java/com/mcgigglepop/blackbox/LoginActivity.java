dfdea4e2fa42e25a6043535d9a1523a8a14d1759
cristopher <christoph.alano@gmail.com>
2022-02-26 00:52:01 -0600

    added the logic for login

--- final/app/src/main/java/com/mcgigglepop/blackbox/LoginActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/LoginActivity.java
@@ -2,8 +2,13 @@ package com.mcgigglepop.blackbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Toast;
 
+import com.mcgigglepop.blackbox.helpers.CustomEditText;
 import com.mcgigglepop.blackbox.helpers.DatabaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,5 +20,30 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         databaseHelper = new DatabaseHelper(LoginActivity.this);
+
+        final CustomEditText editTextLogin =  findViewById(R.id.edit_text_login);
+        editTextLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (databaseHelper.checkPassword(s.toString())) {
+                    Intent intent = new Intent(LoginActivity.this, AccountsActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else{
+                    Toast.makeText(LoginActivity.this, "Incorrect Passcode", Toast.LENGTH_SHORT).show();
+                    editTextLogin.setText(null);
+                }
+            }
+        });
     }
 }
