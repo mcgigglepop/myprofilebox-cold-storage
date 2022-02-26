401d3cf846c7915096a665b4cf6fe12cb974f41c
cristopher <christoph.alano@gmail.com>
2022-02-26 00:48:32 -0600

    added the database helper

--- final/app/src/main/java/com/mcgigglepop/blackbox/LoginActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/LoginActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mcgigglepop.blackbox.helpers.DatabaseHelper;
+
 public class LoginActivity extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        databaseHelper = new DatabaseHelper(LoginActivity.this);
     }
 }
