5050f40e80ee344fea481de4bc60cb6bf0b75dda
cristopher <christoph.alano@gmail.com>
2022-03-05 13:18:05 -0600

    added some database variables

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelperAccounts.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelperAccounts.java
@@ -1,10 +1,19 @@
 package com.mcgigglepop.blackbox.helpers;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class DatabaseHelperAccounts {
     public static final String KEY_ROWID = "_id";
     public static final String KEY_ACCOUNT_TYPE = "atype";
     public static final String KEY_ACCOUNT_NAME = "aname";
     public static final String KEY_USERNAME = "username";
     public static final String KEY_PASSWORD = "password";
+    private static final String DATABASE_NAME = "table";
+    private static final String SQLITE_TABLE = "records";
 
+    private final Context context;
+
+    private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
 }
