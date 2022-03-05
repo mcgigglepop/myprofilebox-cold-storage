151bca60d96c23a5c3ee61e60e99306b709b74c2
cristopher <christoph.alano@gmail.com>
2022-03-05 16:35:15 -0600

    on click listener for the button

--- final/app/src/main/java/com/mcgigglepop/blackbox/CreateAccountActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/CreateAccountActivity.java
@@ -3,12 +3,24 @@ package com.mcgigglepop.blackbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    Button confirm_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        confirm_account = (Button) findViewById(R.id.confirm_create_account_button);
+
+        confirm_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
