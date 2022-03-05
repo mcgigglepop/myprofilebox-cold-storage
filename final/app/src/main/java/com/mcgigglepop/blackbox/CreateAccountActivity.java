540099c4a52ff349b14b74c88b8efbe7dcb5ef2c
cristopher <christoph.alano@gmail.com>
2022-03-05 16:38:10 -0600

    styles and formatting

--- final/app/src/main/java/com/mcgigglepop/blackbox/CreateAccountActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/CreateAccountActivity.java
@@ -19,7 +19,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         confirm_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
