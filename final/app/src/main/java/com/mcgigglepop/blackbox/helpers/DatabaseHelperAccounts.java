10d5083f73a1e0358c0933ace80df4b39ed8f8aa
cristopher <christoph.alano@gmail.com>
2022-03-05 13:20:20 -0600

    variables and constructor

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelperAccounts.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelperAccounts.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DatabaseHelperAccounts {
+
     public static final String KEY_ROWID = "_id";
     public static final String KEY_ACCOUNT_TYPE = "atype";
     public static final String KEY_ACCOUNT_NAME = "aname";
@@ -11,9 +12,17 @@ public class DatabaseHelperAccounts {
     public static final String KEY_PASSWORD = "password";
     private static final String DATABASE_NAME = "table";
     private static final String SQLITE_TABLE = "records";
+    private static final int DATABASE_VERSION =  1;
 
     private final Context context;
 
     private DatabaseHelper dbHelper;
     private SQLiteDatabase db;
+
+    private static class DatabaseHelperAccounts extends SQLiteOpenHelper {
+
+        DatabaseHelperAccounts(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+    }
 }
