3e06bfb23bc3fc599a7480176ce518500e8056d8
cristopher <christoph.alano@gmail.com>
2022-03-05 16:45:19 -0600

    styles and formatting

--- final/app/src/main/java/com/mcgigglepop/blackbox/CreateAccountActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/CreateAccountActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateAccountActivity extends AppCompatActivity {
     Button confirm_account;
+    EditText account_type, account_name, username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,26 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         confirm_account = (Button) findViewById(R.id.confirm_create_account_button);
 
+        account_type = (EditText) findViewById(R.id.account_type_edittext);
+        account_name = (EditText) findViewById(R.id.account_name_edittext);
+        username = (EditText) findViewById(R.id.account_username_edittext);
+        password = (EditText) findViewById(R.id.account_password_edittext);
+
         confirm_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(account_type.getText().toString().length() <= 0){
+                    Toast.makeText(CreateAccountActivity.this, "Account Type must not be empty", Toast.LENGTH_LONG).show();
+                }
+                if(account_name.getText().toString().length() <= 0){
+                    Toast.makeText(CreateAccountActivity.this, "Account Name must not be empty", Toast.LENGTH_LONG).show();
+                }
+                if(username.getText().toString().length() <= 0){
+                    Toast.makeText(CreateAccountActivity.this, "Username must not be empty", Toast.LENGTH_LONG).show();
+                }
+                if(password.getText().toString().length() <= 0){
+                    Toast.makeText(CreateAccountActivity.this, "Password must not be empty", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
