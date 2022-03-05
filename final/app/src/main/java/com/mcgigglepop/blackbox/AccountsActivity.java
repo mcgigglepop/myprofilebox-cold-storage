390b36df0285b4a58780f312b3a7404736556558
cristopher <christoph.alano@gmail.com>
2022-03-05 14:05:09 -0600

    added instantiation for database helper and simple cursor adapter

--- final/app/src/main/java/com/mcgigglepop/blackbox/AccountsActivity.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/AccountsActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.mcgigglepop.blackbox.helpers.DatabaseHelperAccounts;
 
 public class AccountsActivity extends AppCompatActivity {
-    FloatingActionButton fab;
+    private DatabaseHelperAccounts dbHelper;
+    private SimpleCursorAdapter dataAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
+
+        dbHelper = new DatabaseHelperAccounts(this);
+        dbHelper.open();
 
 
     }
