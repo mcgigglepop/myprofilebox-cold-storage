f563bcdb9eb226a2f9c4ce85485614e6c28a339f
cristopher <christoph.alano@gmail.com>
2022-03-05 13:15:43 -0600

    added some strings for account information

--- final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelperAccounts.java
+++ final/app/src/main/java/com/mcgigglepop/blackbox/helpers/DatabaseHelperAccounts.java
@@ -1,4 +1,10 @@
 package com.mcgigglepop.blackbox.helpers;
 
 public class DatabaseHelperAccounts {
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_ACCOUNT_TYPE = "atype";
+    public static final String KEY_ACCOUNT_NAME = "aname";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+
 }
