1d2ef30de1aae5236e8607cef508b461a32588c5
cristopher <christoph.alano@gmail.com>
2022-04-10 14:54:07 -0500

    complete function

--- aws-infrastructure/share-account/lambda-layers/lambda/src/generate-mfa.py
+++ aws-infrastructure/share-account/lambda-layers/lambda/src/generate-mfa.py
@@ -1,15 +1,30 @@
 from doctest import master
 import json
-import boto3
+from random import randint
 
 def lambda_handler(event, context):
-    print("GENERATE MFA")
-    print(event)
+    
+    # inputs
+    record_id = event["record_id"]
+    account_type = event["account_type"]
+    account_username = event["account_username"]
+    account_name = event["account_name"]
 
+    # generate mfa
+    mfa = random_with_N_digits(6)
+    
     result = {
-        "step": 3,
-        "state": "generate mfa"
+        "record_id": record_id,
+        "account_type": account_type,
+        "account_username": account_username, 
+        "account_name": account_name,
+        "mfa": mfa
     }
 
     response = json.dumps(result)
     return response
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
