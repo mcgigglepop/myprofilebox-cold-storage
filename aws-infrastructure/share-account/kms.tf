d0fcce0a628a6dd32d4a9e56051f3523bb71a080
cristopher <christoph.alano@gmail.com>
2022-04-10 16:05:19 -0500

    added the kms key resources

---
+++ aws-infrastructure/share-account/kms.tf
@@ -0,0 +1,17 @@
+// kms resource
+resource "aws_kms_key" "kms_resource" {
+  description = "KMS encryption key for ${var.project}"
+}
+
+// alias for the kms key
+resource "aws_kms_alias" "kms_resource" {
+  name          = "alias/kms_key_alias"
+  target_key_id = "${aws_kms_key.kms_resource.key_id}"
+}
+
+// ssm parameter store for the kms key resource
+resource "aws_ssm_parameter" "kms_resource" {
+  name  = "/${aws_kms_alias.kms_resource.name}"
+  type  = "SecureString"
+  value = "${aws_kms_key.kms_resource.arn}"
+}
