bac78d7937efc3c5467172c50ea4b6ffcde37a48
cristopher <christoph.alano@gmail.com>
2022-04-10 15:59:30 -0500

    added server side encryption configuration

--- aws-infrastructure/share-account/s3.tf
+++ aws-infrastructure/share-account/s3.tf
@@ -2,3 +2,14 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket="${var.project}-encrypted"
 }
+
+# S3 Bucket Server Side Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse_config" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
