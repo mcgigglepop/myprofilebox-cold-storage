8ae591502fca68aa819079cfaa256ac55efe1a55
cristopher <christoph.alano@gmail.com>
2022-04-10 03:20:03 -0500

    added the iam policy attachments

--- aws-infrastructure/share-account/iam.tf
+++ aws-infrastructure/share-account/iam.tf
@@ -94,7 +94,7 @@ resource "aws_iam_policy" "create_dynamo_record_lambda_policy" {
 EOF
 }
 
-# IAM Policy for the Store in S3 Function
+# IAM Policy for the Send Text Message Function
 resource "aws_iam_policy" "send_text_message_lambda_policy" {
   name = "${var.project}-send-text-message-lambda-policy"
   description = "IAM Policy for the Send Text Message Lambda Function"
@@ -116,4 +116,34 @@ resource "aws_iam_policy" "send_text_message_lambda_policy" {
   ]
 }
 EOF
+}
+
+# Policy Attachment for the Encrypt Secret Lamdba Function
+resource "aws_iam_role_policy_attachment" "encrypt_secret_lambda_policy_attachment" {
+  role = module.encrypt-secret-lambda.role_name
+  policy_arn = aws_iam_policy.encrypt_secret_lambda_policy.arn
+}
+
+# Policy Attachment for the Store in S3 Lamdba Function
+resource "aws_iam_role_policy_attachment" "store_in_s3_lambda_policy_attachment" {
+  role = module.store-in-s3-lambda.role_name
+  policy_arn = aws_iam_policy.store_in_s3_lambda_policy.arn
+}
+
+# Policy Attachment for the Generate MFA Lamdba Function
+resource "aws_iam_role_policy_attachment" "generate_mfa_lambda_policy_attachment" {
+  role = module.generate-mfa-lambda.role_name
+  policy_arn = aws_iam_policy.generate_mfa_lambda_policy.arn
+}
+
+# Policy Attachment for the Create Dynamo Record Lamdba Function
+resource "aws_iam_role_policy_attachment" "create_dynamo_record_lambda_policy_attachment" {
+  role = module.create-dynamo-record-lambda.role_name
+  policy_arn = aws_iam_policy.create_dynamo_record_lambda_policy.arn
+}
+
+# Policy Attachment for the Send Text Message Lamdba Function
+resource "aws_iam_role_policy_attachment" "send_text-message_lambda_policy_attachment" {
+  role = module.send-text-message-lambda.role_name
+  policy_arn = aws_iam_policy.send_text_message_lambda_policy.arn
 }
