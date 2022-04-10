280aff4a86afe0b025d41aa601bea7bfd2da8e6b
cristopher <christoph.alano@gmail.com>
2022-04-10 17:59:33 -0500

    added depends on

--- aws-infrastructure/share-account/api.tf
+++ aws-infrastructure/share-account/api.tf
@@ -7,6 +7,7 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
 resource "aws_api_gateway_deployment" "deployment" {
     rest_api_id   = "${aws_api_gateway_rest_api.api_gateway.id}"
     stage_name    = "${var.stage}"
+    depends_on = ["aws_api_gateway_method.encrypt_method", "aws_api_gateway_integration.encrypt_integration"]
 }
 
 # encrypt Resource
