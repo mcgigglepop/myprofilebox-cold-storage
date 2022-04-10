40a76eafa7cdbf6fb392db23e2c06d438e40e8cf
cristopher <christoph.alano@gmail.com>
2022-04-10 14:50:30 -0500

    complete function

--- aws-infrastructure/share-account/lambda-layers/lambda/src/store-in-s3.py
+++ aws-infrastructure/share-account/lambda-layers/lambda/src/store-in-s3.py
@@ -1,14 +1,30 @@
 from doctest import master
 import json
 import boto3
+import os
 
 def lambda_handler(event, context):
-    print("STORE IN S3")
-    print(event)
+    # top-level variables
+    s3 = boto3.resource("s3")
+    bucket_name = os.environ['S3_BUCKET']    
+
+    # inputs
+    record_id = event["record_id"]
+    encoded_password = event["encoded_password"]
+    account_type = event["account_type"]
+    account_username = event["account_username"]
+    account_name = event["account_name"]
+
+    # store encrypted secret in s3
+    encoded_password_string = encoded_password.decode("utf-8")
+    s3.Bucket(bucket_name).put_object(Key=record_id, Body=encoded_password_string)
+    
 
     result = {
-        "step": 2,
-        "state": "store in s3"
+        "record_id": record_id,
+        "account_type": account_type,
+        "account_username": account_username, 
+        "account_name": account_name
     }
 
     response = json.dumps(result)
