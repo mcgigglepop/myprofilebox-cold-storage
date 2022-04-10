e3758c80aa0ba36f9dd92f317b3827bc4db052c6
cristopher <christoph.alano@gmail.com>
2022-04-10 15:42:05 -0500

    stringified numbers

--- aws-infrastructure/share-account/lambda-layers/lambda/src/send-text-message.py
+++ aws-infrastructure/share-account/lambda-layers/lambda/src/send-text-message.py
@@ -12,8 +12,8 @@ def lambda_handler(event, context):
     phone_number = event["phone_number"]
 
     # send text
-    number = phone_number
-    message = "Blackbox message. Use MFA code " + mfa + " to retrieve your secret \n\n https://blackbox.com/view/"+ record_id
+    number = str(phone_number)
+    message = "Blackbox message. Use MFA code " + str(mfa) + " to retrieve your secret \n\n https://blackbox.com/view/"+ str(record_id)
     sns.publish(PhoneNumber = number, Message=message )
     
     result = {
