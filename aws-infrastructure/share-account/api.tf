af151a3aa5cd6b9ca7cc53bca79a1804cc24c8f2
cristopher <christoph.alano@gmail.com>
2022-04-10 17:37:37 -0500

    added cors options method

--- aws-infrastructure/share-account/api.tf
+++ aws-infrastructure/share-account/api.tf
@@ -47,3 +47,11 @@ resource "aws_api_gateway_integration" "encrypt_integration" {
 EOF
   }
 }
+
+# CORS OPTIONS Method for the encrypt Endpoint
+resource "aws_api_gateway_method" "cors_method_encrypt" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api_gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.encrypt_resource.id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
