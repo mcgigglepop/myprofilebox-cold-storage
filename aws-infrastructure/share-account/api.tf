f70fac2aa4292492c5c4ced78d74b05cd4ce8632
cristopher <christoph.alano@gmail.com>
2022-04-10 17:30:36 -0500

    added encrypt resource

--- aws-infrastructure/share-account/api.tf
+++ aws-infrastructure/share-account/api.tf
@@ -8,3 +8,10 @@ resource "aws_api_gateway_deployment" "deployment" {
     rest_api_id   = "${aws_api_gateway_rest_api.api_gateway.id}"
     stage_name    = "${var.stage}"
 }
+
+# encrypt Resource
+resource "aws_api_gateway_resource" "encrypt_resource" {
+  path_part   = "encrypt"
+  parent_id   = "${aws_api_gateway_rest_api.api_gateway.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api_gateway.id}"
+}
