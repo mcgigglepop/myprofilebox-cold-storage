7f0546f3457d68be4d5b2787614ff7db2772e9c6
cristopher <christoph.alano@gmail.com>
2022-04-10 16:10:07 -0500

    removed unused resource

--- aws-infrastructure/share-account/kms.tf
+++ aws-infrastructure/share-account/kms.tf
@@ -8,10 +8,3 @@ resource "aws_kms_alias" "kms_resource" {
   name          = "alias/${var.project}_kms_key_alias"
   target_key_id = "${aws_kms_key.kms_resource.key_id}"
 }
-
-// ssm parameter store for the kms key resource
-resource "aws_ssm_parameter" "kms_resource" {
-  name  = "/${aws_kms_alias.kms_resource.name}"
-  type  = "SecureString"
-  value = "${aws_kms_key.kms_resource.arn}"
-}
