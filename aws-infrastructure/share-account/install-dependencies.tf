9325abae6dfdf0f8bb0b269246f6c642657d2397
cristopher <christoph.alano@gmail.com>
2022-04-10 02:39:03 -0500

    changed directory source and location for install dependencies

--- aws-infrastructure/share-account/install-dependencies.tf
+++ aws-infrastructure/share-account/install-dependencies.tf
@@ -1,5 +1,5 @@
 data "archive_file" "create_dist_pkg" {
-  source_dir = "${path.cwd}/../lambda-layers/lambda/src"
-  output_path = "${path.cwd}/../lambda-layers/lambda/packages/lambda_function.zip"
+  source_dir = "${path.cwd}/lambda-layers/lambda/src"
+  output_path = "${path.cwd}/lambda-layers/lambda/packages/lambda_function.zip"
   type = "zip"
 }
