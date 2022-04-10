c2fe410e61c4fb3f557723db434dfb1b09211fb2
cristopher <christoph.alano@gmail.com>
2022-04-10 02:28:06 -0500

    outputs for lambda module

---
+++ aws-infrastructure/share-account/modules/lambda/outputs.tf
@@ -0,0 +1,11 @@
+output "arn" {
+  value = aws_lambda_function.lambda.arn
+}
+
+output "role_name" {
+  value = aws_iam_role.lambda_role.name
+}
+
+output "function_name" {
+  value = aws_lambda_function.lambda.function_name
+}
